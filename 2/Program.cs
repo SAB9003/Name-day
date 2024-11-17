82dab92215bc2c2289fb18bbfdad31a9583a05bf
Alex9003 <agl.luchko@gmail.com>
2024-11-17 22:37:20 +0200

    Add project files.

---
+++ 2/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input day of the week number:");
+int day = Convert.ToInt32(Console.ReadLine());
+//2 Варіан закоментований
+if (day == 0 || day > 7){
+    Console.WriteLine("Error,there is no such day");
+}
+
+else {
+    string day_name = day switch
+    {
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday",
+        5 => "Friday",
+        6 => "Saturday",
+        7 => "Sunday"
+    };
+    Console.WriteLine($"It is", day_name);
+}
+/*switch (day)
+{
+    case 1:
+        Console.WriteLine("It is Monday");
+        break;
+    case 2: 
+        Console.WriteLine("It is Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("It is Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("It is Thursday");
+        break;
+    case 5:
+        Console.WriteLine("It is Friday");
+        break;
+    case 6:
+        Console.WriteLine("It is Saturday");
+        break;
+    case 7:
+        Console.WriteLine("It is Sunday");
+        break;
+    default:
+        Console.WriteLine("Error,there is no such day");
+        break;
+}*/
---
+++ calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is a calculator");
+Console.WriteLine("Input first numder:");
+int forst_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input sign:");
+string? sign = Console.ReadLine();
+Console.WriteLine("Input second numder:");
+int second_number = Convert.ToInt32(Console.ReadLine());
+if ((sign == "/" || sign=="%") && second_number == 0)
+{
+    Console.WriteLine("Error, you cannot divide by 0");
+}
+else
+{
+    switch(sign)
+    {
+        case "+":
+            Console.WriteLine($"{forst_number} + {second_number} = {forst_number+second_number}");
+            break;
+        case "-":
+            Console.WriteLine($"{forst_number} - {second_number} = {forst_number - second_number}");
+            break;
+        case "*":
+            Console.WriteLine($"{forst_number} * {second_number} = {forst_number * second_number}");
+            break;
+        case "/":
+            Console.WriteLine($"{forst_number} / {second_number} = {(double)forst_number / second_number}");
+            break;
+        case "%":
+            Console.WriteLine($"{forst_number} % {second_number} = {forst_number % second_number}");
+            break;
+    }
+}
