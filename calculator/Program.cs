3ac6bf58d835c2971a781d65deaa447d181e086b
Alex9003 <agl.luchko@gmail.com>
2024-11-24 23:17:39 +0200

    Use try and catch

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,33 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This is a calculator");
-Console.WriteLine("Input first numder:");
-int forst_number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input sign:");
-string? sign = Console.ReadLine();
-Console.WriteLine("Input second numder:");
-int second_number = Convert.ToInt32(Console.ReadLine());
-if ((sign == "/" || sign=="%") && second_number == 0)
+try
 {
-    Console.WriteLine("Error, you cannot divide by 0");
-}
-else
-{
-    switch(sign)
+    Console.WriteLine("This is a calculator");
+
+    Console.WriteLine("Input first number:");
+    string firstInput = Console.ReadLine();
+    int first_number = Convert.ToInt32(firstInput);
+
+    Console.WriteLine("Input sign:");
+    string? sign = Console.ReadLine();
+
+    Console.WriteLine("Input second number:");
+    string secondInput = Console.ReadLine();
+    int second_number = Convert.ToInt32(secondInput);
+    if ((sign == "/" || sign == "%") && second_number == 0)
+    {
+        throw new DivideByZeroException("Error: you cannot divide by 0");
+    }
+
+    double result;
+
+    switch (sign)
     {
         case "+":
-            Console.WriteLine($"{forst_number} + {second_number} = {forst_number+second_number}");
+            Console.WriteLine($"{first_number} + {second_number} = {first_number + second_number}");
             break;
         case "-":
-            Console.WriteLine($"{forst_number} - {second_number} = {forst_number - second_number}");
+            Console.WriteLine($"{first_number} - {second_number} = {first_number - second_number}");
             break;
         case "*":
-            Console.WriteLine($"{forst_number} * {second_number} = {forst_number * second_number}");
+            Console.WriteLine($"{first_number} * {second_number} = {first_number * second_number}");
             break;
         case "/":
-            Console.WriteLine($"{forst_number} / {second_number} = {(double)forst_number / second_number}");
+            Console.WriteLine($"{first_number} / {second_number} = {(double)first_number / second_number}");
             break;
         case "%":
-            Console.WriteLine($"{forst_number} % {second_number} = {forst_number % second_number}");
+            Console.WriteLine($"{first_number} % {second_number} = {first_number % second_number}");
             break;
+        default:
+            throw new InvalidOperationException("Error: invalid operation");
     }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: Incorrectly entered number");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Error: you cannot divide by 0.");
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected error: {ex.Message}");
 }
